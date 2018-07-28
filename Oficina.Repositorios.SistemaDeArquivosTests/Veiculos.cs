de541a97f6955f84e51d717e34d2e9b5fc8ef241
elton .fox <elton.fox@hotmail.com>
2018-07-28 17:33:16 -0300

    Aula28/07

---
+++ Oficina.Repositorios.SistemaDeArquivosTests/Veiculos.cs
@@ -0,0 +1,30 @@
+﻿using Oficina.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficina.Repositorios.SistemaDeArquivosTests
+{
+    class Veiculos
+    {
+        //public Veiculo()
+        //{
+        //    Id = Guid.NewGuid();
+        //}
+
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Placa { get; set; }
+        public int Ano { get; set; }
+        public string Observacao { get; set; }
+
+        public Modelo Modelo { get; set; }
+        public Cor Cor { get; set; }
+
+        public Combustivel Combustivel { get; set; }
+        public Cambio Cambio { get; set; }
+
+    }
+}
